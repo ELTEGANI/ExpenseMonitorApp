4634284214daf3a0fea646a6e26dfb458f3bb429
Tiganimohammad <tiganimoh@gmail.com>
2020-01-04 23:59:42 +0200

    added new domain with ssl config https://www.expensemonitor.app/

--- app/src/main/java/com/expensemoitor/expensemonitor/utilites/AppConstants.kt
+++ app/src/main/java/com/expensemoitor/expensemonitor/utilites/AppConstants.kt
@@ -1,7 +1,7 @@
 package com.expensemoitor.expensemonitor.utilites
 
 object AppConstants {
-    const val BASEURL = "http://104.207.131.106/"
+    const val BASEURL = "https://www.expensemonitor.app/"
     const val USER_REGISTERATION = "api/user/registeruser"
     const val CREATE_NEW_EXPENSE = "api/expense/createexpense"
     const val GET_EXPENSES_BASED_ON_DURATION = "api/expense/getExpensesBasedOnDuration"
