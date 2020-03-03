0e9b90083da1afcb12ba255d2b7f93aded1f7765
Tiganimohammad <tiganimoh@gmail.com>
2020-03-03 08:08:23 +0200

    adds test cahces

--- app/src/main/java/com/monitoryourexpenses/expenses/database/LocalRepository.kt
+++ app/src/main/java/com/monitoryourexpenses/expenses/database/LocalRepository.kt
@@ -67,10 +67,10 @@ class LocalRepository(private val database:ExpenseMonitorDao) {
                         val expensesList = ApiFactory.ALL_EXPENSES.allExpensesAsync(duration).await()
                         database.insertExpensesForCachingData(*expensesList.toTypedArray())
                     }catch (http:HttpException){
-                      Log.d("http",http.toString())
+                      Log.d("httpException",http.toString())
                     }
                 }catch (t:Throwable){
-                    Log.d("t",t.toString())
+                    Log.d("Throwable",t.toString())
                 }
             }
         }
