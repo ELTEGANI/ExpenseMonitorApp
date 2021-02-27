84a7f4b3460895a8dbf8e2ccf0ec2ca2c718d4bc
altegani mohammed <tiganimoh@gmail.com>
2021-02-27 19:18:02 +0200

    add mark dawn

--- app/src/main/java/com/monitoryourexpenses/expenses/MainActivity.kt
+++ app/src/main/java/com/monitoryourexpenses/expenses/MainActivity.kt
@@ -19,9 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        AppCenter.start(
-            application, "da6402fe-c7ef-4073-8b1f-7149be87e03e",
-            Analytics::class.java, Crashes::class.java)
+        AppCenter.start(application, "da6402fe-c7ef-4073-8b1f-7149be87e03e",Analytics::class.java, Crashes::class.java)
 
     }
 }
